768533b4d3d6a144b62bbe3549929cc0e2e3e6e1
Christian <ianalis@gmail.com>
2019-03-11 08:00:06 +0800

    Implement filtered stream

--- stwark.py
+++ stwark.py
@@ -11,15 +11,17 @@ from requests.exceptions import ChunkedEncodingError, ConnectionError
 from OpenSSL.SSL import ZeroReturnError
 
 class OutputStream(object):
-    def __init__(self, archive_dir, prefix='data'):
+    def __init__(self, working_dir, archive_dir, prefix='data'):
+        self.working_dir = working_dir
         self.archive_dir = archive_dir
-        if not os.path.exists(archive_dir):
-            os.makedirs(archive_dir)
+        if not os.path.exists(working_dir):
+            os.makedirs(working_dir)
         self.prefix = prefix
         self.curhour = None
         self._outfile = None
+        self._outfile_name = None
         
-    def restart(self, base_datetime):
+    def restart(self, base_datetime, move_current=False):
         """
         Start a new hourly output tarball.
         
@@ -33,19 +35,18 @@ class OutputStream(object):
         if self._outfile:
             self._outfile.close()
 
-        if move_current:
-            if not os.path.exists(self.archive_dir):
-                os.makedirs(self.archive_dir)
-            os.replace(f.name,
-                       os.path.join(self.archive_dir, 
-                                    os.path.basename(f.name)))
+            if move_current:
+                if not os.path.exists(self.archive_dir):
+                    os.makedirs(self.archive_dir)
+                os.replace(self._outfile_name,
+                           os.path.join(self.archive_dir, 
+                                        os.path.basename(self._outfile_name)))
 
         self.curhour = base_datetime.replace(minute=0, second=0, microsecond=0)
-        self._outfile = BZ2File(os.path
-                                  .join(self.working_dir,
-                                        self.curhour
-                                            .strftime(self.prefix+'-%y%m%d%H.json.bz2')),
-                                'a')
+        self._outfile_name = os.path.join(
+            self.working_dir,
+            self.curhour.strftime(self.prefix+'-%y%m%d%H.json.bz2'))
+        self._outfile = BZ2File(self._outfile_name,'a')
         
     def write(self, data):
         self._outfile.write((json.dumps(data) + '\r\n').encode('utf8'))        
@@ -63,7 +64,7 @@ class SampleStreamer(TwythonStreamer):
             created_at = datetime.strptime(data['created_at'], 
                                            '%a %b %d %H:%M:%S +0000 %Y') 
             if created_at > self.outstream.curhour + timedelta(hours=1):
-                self.outstream.restart(created_at)
+                self.outstream.restart(created_at, move_current=True)
         self.outstream.write(data)
 
     def on_error(self, status_code, data):
@@ -79,6 +80,9 @@ def read_settings(args_source=sys.argv[1:]):
     parser.add_argument('oauth_secret', 
                         nargs='?',
                         help='User OAuth secret')
+    parser.add_argument('--filter',
+                        action='store_true',
+                        help='Follow filtered stream instead of sample')
     parser.add_argument('-p', '--prefix',
                         help='Name to start filenames with (default: data)')
     parser.add_argument('--config',
@@ -111,11 +115,19 @@ def read_settings(args_source=sys.argv[1:]):
         sys.exit(-1)
     
     return settings
-    
+
+def file2param(text_file):
+    """Convert text file to comma-separated string"""
+    params = [line.strip() for line in open(text_file).readlines()]
+    return ','.join([line for line in params if line])
+
 if __name__ == "__main__":
     settings = read_settings()
     
-    outstream = OutputStream('data/sample', settings['prefix'])
+    if settings['filter']:
+        outstream = OutputStream('.', 'data/filter', settings['prefix'])
+    else:
+        outstream = OutputStream('.', 'data/sample', settings['prefix'])
     outstream.restart(datetime.utcnow())
     while True:
         try:
@@ -124,6 +136,12 @@ if __name__ == "__main__":
                                       settings['oauth_token'], 
                                       settings['oauth_secret'],
                                       outstream)
-            streamer.statuses.sample()
+            if settings['filter']:
+                params = {}
+                for key in ['track', 'locations']:
+                    params[key] = file2param(key+'.txt')
+                streamer.statuses.filter(**params)
+            else:
+                streamer.statuses.sample()
         except (ChunkedEncodingError, ConnectionError, ZeroReturnError):
             pass
